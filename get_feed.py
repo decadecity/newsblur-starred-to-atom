5f9fa87fa8397c384d7a68ace5f9f5ded95d4e3d
Orde Saunders <orde@decadecity.net>
2014-10-27 10:54:37 +0000

    Feed validation.

--- get_feed.py
+++ get_feed.py
@@ -2,15 +2,21 @@ import newsblur
 import feedgenerator
 import config
 
+from datetime import datetime
+
+def get_date(string, fmt="%Y-%m-%d %H:%M:%S"):
+    return datetime.strptime(string, fmt)
+
 api = newsblur.API()
 api.login(config.username, config.password)
 stories = api.starred_stories()
 
 feed = feedgenerator.Atom1Feed(
     title=u"newsblur starred stories",
-    link=u"http://example.com",
+    link=u"http://example.com/",
     description=u"Stuff",
     language=u"en-GB",
+    author=u'Fnord',
 )
 
 for story in stories['stories']:
@@ -18,6 +24,8 @@ for story in stories['stories']:
         title=story['story_title'],
         link=story['story_permalink'],
         description=story['story_content'],
+        author_name=story['story_authors'],
+        pubdate=get_date(story['story_date']),
     )
 
 print(feed.writeString('utf-8'))
